4724c1f66df35e849e860cd9047aac36eda7420a
danjsiegel <dan.j.siegel@gmail.com>
2017-01-22 14:29:18 -0800

    fixed error which was carrots in wrong direction

--- lab2/realtor.cpp
+++ lab2/realtor.cpp
@@ -15,9 +15,9 @@
   {
     // Declaration of Variables:
     string lastName;
-    double cost_of_house, price_to_sell, price_of_comission
-    const double percentage_to_sell = .06 * cost_of_house;
-    const double percentage_of_comission = .25 * cost_to_sell;
+    double cost_of_house;
+    const double cost_to_sell = 0.06 * cost_of_house;
+    const double comission = 0.25 * cost_to_sell;
 
     // Collects Last Name
     cout << "Please enter the owner’s last name: ";
@@ -25,9 +25,9 @@
     cin >> lastName;
     cout << "Please enter the sales price of the home:";
     cin >> cost_of_house;
-    cout >> "The " >> lastName >> "'s\' home sold for "  >> cost_of_house >> endl;
-    cout >> "The cost to sell the home is" >> cost_to_sell >> endl;
-    cout >> "The selling or listing agent commission is " >> comission >> endl;
-    
+    cout << "The " << lastName << "\'s home sold for "  << cost_of_house << endl;
+    cout << "The cost to sell the home is" << cost_to_sell << endl;
+    cout << "The selling or listing agent commission is " << comission << endl;
+
     return 0;
-}
+  }
