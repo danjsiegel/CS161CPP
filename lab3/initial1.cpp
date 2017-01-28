298c16d8405d445888d659cfc1b6b137e5fc2b35
danjsiegel <dan.j.siegel@gmail.com>
2017-01-28 15:47:39 -0800

    started first assignment

---
+++ lab3/initial1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  string first, middle, last
+  cout << "Enter your first name, middle name or initial, and last name separated by spaces: Jane Ann Do" << '\n';
+  first = cin.get();
+    cin.ignore(256, ' ');
+  middle = cin.get();
+   cin.ignore(256,' ');
+  last = cin.get();
+   cout << '\n' << first << middle << last << '\n';
+  return 0;
+}
