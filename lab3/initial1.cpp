05240d572d7f9836084c0ee7615b307141a40c2c
danjsiegel <dan.j.siegel@gmail.com>
2017-01-28 15:53:39 -0800

    added code comments

--- lab3/initial1.cpp
+++ lab3/initial1.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-  string first, middle, last;
+  string first, middle, last
   cout << "Enter your first name, middle name or initial, and last name separated by spaces: Jane Ann Do" << '\n';
-  first = cin.get();
-    cin.ignore(256, ' ');
-  middle = cin.get();
-   cin.ignore(256,' ');
+  first = cin.get(); //cin.get grabs the first character of the input
+    cin.ignore(15, ' '); //tell's the input to ignore until either 15 characters or until a white space character.
+  middle = cin.get(); //grabs the first character of the input
+   cin.ignore(15,' '); //ignores the first 15 characters or until a white space character.
   last = cin.get();
-   cout << '\n' << first << middle << last << '\n';
+   cout << '\n' << first << middle << l << '\n'; //prints the initials
   return 0;
 }
