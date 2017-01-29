f5981108f2cd7ec183017fcebc046850842d5fae
Dan <dan.j.siegel@gmail.com>
2017-01-28 16:12:59 -0800

    updated files again

--- lab3/initial1.cpp
+++ lab3/initial1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
   string first, middle, last;
-  cout << "Enter your first name, middle name or initial, and last name separated by spaces: Jane Ann Do" << '\n';
+  cout << "Enter your first name, middle name or initial, and last name separated by spaces:" << '\n';
   first = cin.get(); //cin.get grabs the first character of the input
     cin.ignore(15, ' '); //tell's the input to ignore until either 15 characters or until a white space character.
   middle = cin.get(); //grabs the first character of the input
