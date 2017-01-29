caebad1a17307cc928d065abcd0e36c4392e039c
danjsiegel <dan.j.siegel@gmail.com>
2017-01-28 16:24:38 -0800

    worked on initial2.cpp

--- lab3/initial2.cpp
+++ lab3/initial2.cpp
@@ -1,14 +1,27 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main(){
-  string first, middle, last;
-  cout << "Enter your first name, middle name or initial, and last name separated by spaces: Jane Ann Do" << '\n';
-  first = cin.get(); //cin.get grabs the first character of the input
-    cin.ignore(15, ' '); //tell's the input to ignore until either 15 characters or until a white space character.
-  middle = cin.get(); //grabs the first character of the input
-   cin.ignore(15,' '); //ignores the first 15 characters or until a white space character.
-  last = cin.get();
-   cout << '\n' << first << middle << last << '\n'; //prints the initials
+  string firstIn, middleIn, lastIn; //initials
+  string firstNm, middleNm, lastNm; //Whole Name
+  cout << "Enter your first name, middle name or initial, and last name separated by spaces:" << '\n';
+  //First Name
+  firstIn = cin.get(); //cin.get grabs the first character of the input
+  cin.putback(firstIn); //Put's back the value grabbed in first variable
+  cin >> firstNm;
+  //Middle Name
+  cin.ignore(' '); //tell's the input to ignore white space character.
+  middleIn = cin.get(); //grabs the first character of the input
+  cin.putback(middleIn);
+  cin >> middleNm;
+  // Last Name
+  cin.ignore(' '); //ignores the first 15 characters or until a white space character.
+  lastIn = cin.get();
+  cin.putback(lastIn);
+  cin >> lastNm;
+
+  cout << "\n Your Full Name is " << firstNm << middleNm << lastNm << '\n'
+  cout << "\n Your Initials are:" << firstIn << middleIn << lastIn << '\n'; //prints the initials
   return 0;
 }
