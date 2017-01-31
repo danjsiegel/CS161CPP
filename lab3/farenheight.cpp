178560674000d0204d58d4a80a61f7bbf181dcd2
danjsiegel <dan.j.siegel@gmail.com>
2017-01-30 19:47:06 -0800

    syntax error

--- lab3/farenheight.cpp
+++ lab3/farenheight.cpp
@@ -4,7 +4,7 @@
 int main() {
   string date;
   double highTemp, lowTemp, averageTemp;
-  ofstream out("temps.txt")
+  ofstream out("temps.txt");
   cout << "Enter the day with dashes ex: 11-12-16" << endl;
   cin >> date;
   cout << "Enter a hight and a low temperature separated by a space" << endl;
