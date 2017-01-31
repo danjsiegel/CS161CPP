01d459a2cf5551911fbd1bf97ae3794f541169f5
Dan <dan.j.siegel@gmail.com>
2017-01-30 19:56:45 -0800

    compiled farenheight and exported to temps.txt

--- lab3/farenheight.cpp
+++ lab3/farenheight.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+using namespace std; 
 
 int main() {
   string date;
   double highTemp, lowTemp, averageTemp;
-  ofstream out("temps.txt");
+  
+  ofstream out("temps.txt"); //declaration of the output file 
+  
   cout << "Enter the day with dashes ex: 11-12-16" << endl;
   cin >> date;
   cout << "Enter a hight and a low temperature separated by a space" << endl;
   cin >> highTemp >> lowTemp;
-  averageTemp = (highTemp + lowTemp) / 2;
-  cout << date << highTemp << lowTemp << averageTemp;
+  averageTemp = (highTemp + lowTemp)/2;
+  
+  out << date << '\t' << highTemp << '\t' << lowTemp << '\t' << averageTemp << endl; //instead of cout, out is used which writes to the declared file. If non exists, it creates it. 
   return 0;
 }
