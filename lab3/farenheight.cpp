b8c67f986c214fe6205c4d095f92e2e15b782dc3
danjsiegel <dan.j.siegel@gmail.com>
2017-01-30 19:46:23 -0800

    added farenheight with test of my input and output without writing to file

---
+++ lab3/farenheight.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+  string date;
+  double highTemp, lowTemp, averageTemp;
+  ofstream out("temps.txt")
+  cout << "Enter the day with dashes ex: 11-12-16" << endl;
+  cin >> date;
+  cout << "Enter a hight and a low temperature separated by a space" << endl;
+  cin >> highTemp >> lowTemp;
+  averageTemp = (highTemp + lowTemp) / 2;
+  cout << date << highTemp << lowTemp << averageTemp;
+  return 0;
+}
