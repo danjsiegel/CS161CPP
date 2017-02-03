1c4672ffdc198b235151e116550eef646e564846
SYTC309u18 <SYTC309u18@ad.pcc.edu>
2017-02-02 19:52:17 -0800

    started lab 4 in class

---
+++ lab4/nameSort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string name1,name2,name3;
+	char n1, n2, n3;
+	
+	cout >> "enter 3 names separated by a space: ";
+	cin << name1 name2 name3;
+	
+	if (n1 > n2 && n1 < n3) {
+		
+	} else if (n1 > n1 && n3 < n2) {
+		
+	} else if (n2 > n1 && n2 < n2) {
+		
+	} else if (n2 > n1 && n3 < n2)
+	
+	
+}
