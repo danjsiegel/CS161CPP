73f42471a069a7ed3fd0854bbdb1d2cbbcd67313
danjsiegel <dan.j.siegel@gmail.com>
2017-02-05 13:33:24 -0800

    used setup from class to create name sorter

--- lab4/nameSort.cpp
+++ lab4/nameSort.cpp
@@ -4,18 +4,34 @@ using namespace std;
 
 int main(){
 	string name1,name2,name3;
-	char n1, n2, n3;
-	
-	cout >> "enter 3 names separated by a space: ";
-	cin << name1 name2 name3;
-	
-	if (n1 > n2 && n1 < n3) {
-		
-	} else if (n1 > n1 && n3 < n2) {
-		
-	} else if (n2 > n1 && n2 < n2) {
-		
-	} else if (n2 > n1 && n3 < n2)
-	
-	
+	string first, middle, last;
+
+	cout << "enter 3 names separated by a space: ";
+	cin >> name1 >> name2 >> name3;
+	cout << endl;
+	//First Names
+	if (name1 < name2 && name1 < name3) {
+		first = name1;
+	} else if (name2 < name1 && name2 < name3) {
+		first = name2;
+	} else {
+		first = name3;
+	}
+	if (name1 > name2 && name1 > name3) {
+		last = name1;
+	} else if (name2 > name1 && name2 > name3) {
+		last = name2;
+	} else {
+		last = name 3;
+	}
+	if (first < name1 && name1 < last) {
+		middle = name1;
+	} else if (first < name2 && name2 < last) {
+		middle = name2;
+	} else {
+		middle = name3;
+	}
+
+	cout << first << '\n' << middle << '\n' << last << endl;
+	return 0;
 }
