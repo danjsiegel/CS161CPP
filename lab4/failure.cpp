9ee0d85aa304e040b9872668d603a7efff6a493e
danjsiegel <dan.j.siegel@gmail.com>
2017-02-05 14:31:02 -0800

    first whack ath failure.cpp

---
+++ lab4/failure.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+  char initial;
+  int age;
+
+  cout << "Enter your age and your middle initial: ";
+  cin >> age >> initial;
+
+  if (!age) {
+    cout << "your age is bad" << endl;
+  } else {
+    cout << age << end;
+  }
+
+  if (initial != [a-z] || initial != [A-Z]) {
+    cout << "your initial is bad" << endl;
+  } else {
+    cout >> initial << endl;
+  }
+
+}
