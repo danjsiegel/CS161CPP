644103a9698d46dcfafad87c307b7dd7241c83c4
Dan <dan.j.siegel@gmail.com>
2017-02-05 14:04:25 -0800

    got it

--- lab4/nameSort.cpp
+++ lab4/nameSort.cpp
@@ -14,19 +14,19 @@ int main(){
 	} else {
 		cout << name3 << endl;
 	}
-	if (name1 > name2 && name1 > name3) {
+	if (name1 > name2 && name1 < name3) {
 		cout << name1 << endl;
-	} else if (name2 > name1 && name2 > name3) {
+	} else if (name2 < name1 && name2 > name3) {
 		cout << name2 << endl;
 	} else {
 		cout << name3 << endl;
 	}
-	if (first != name1 && last != name1) {
-		cout << name1 << endl;
-	} else if (first != name2 && last != name2) {
+	if (name3 > name2 && name3 > name2) {
+		cout << name3 << endl;
+	} else if (name2 > name1 && name2 > name3) {
 		cout << name2 << endl;
 	} else {
-		cout << name3 << endl;
+		cout << name1 << endl;
 	}
 
 	return 0;
