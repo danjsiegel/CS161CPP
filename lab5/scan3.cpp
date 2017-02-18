73c895595122c26d9da82fcce94ea8f24c1037e8
Dan <dan.j.siegel@gmail.com>
2017-02-18 12:46:59 -0800

    got scan3 to work

--- lab5/scan3.cpp
+++ lab5/scan3.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <iomanip>
+#include <sstream>
 
 using namespace std;
 
 int main() {
-  string dataOfFile;
-  double costOfItem, sumOfItems;
-  ifstream invoiceFile("invoice1.txt");
-  
+  string nameofItem, lineitem, price;
+  int count = 0;
+  double totalPrice = 0;
+  double tempPrice;
+    	
+	cout << "Invoice:" << endl;
+	ifstream invoiceFile("invoice1.txt");
+	while (!invoiceFile.eof()) {
+	getline(invoiceFile, lineitem, '#');
+	getline(invoiceFile, price);
 	
-	while(!invoiceFile.eof()) {
-		invoiceFile >> costOfItem;
-		sumOfItems = sumOfItems + costOfItem;
+	istringstream iss(price);
+	iss >> tempPrice;
+	totalPrice = totalPrice + tempPrice;
 		
+	count = count + 1; 
+	cout << setw (10);
+	cout << lineitem << " " << price << endl;
 	}
-	invoiceFile.close();
-	cout << invoiceFile.rdbuf() << endl;
-	cout << "total" << sumOfItems << endl;
+	cout << fixed << setprecision(2);
+	cout << "The total of " << count << " items is: " << totalPrice << endl;
+	
   return 0;
 }
