d92286690e968a05bde1bf90ff18339cc50788f0
201604 40986 Daniel Siegel <daniel.siegel@syccuxas01.pcc.edu>
2017-02-23 18:03:13 -0800

    added both a regular function and a void function

--- lab6/Payroll2.cpp
+++ lab6/Payroll2.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 using namespace std;
 
-void grossPay(double payRate, int hoursWorked);
+double grossPay(double payRate, int hoursWorked);
 void netPay(double grossAmt, double taxRate);
 
 int main()
@@ -23,8 +23,8 @@ int main()
 		istringstream iss(line);
 		iss >> rate >> hours >> tax;
 		gross = grossPay(rate, hours);
-		net = netPay(gross, tax); 
-		//name = line.substr(0, line.find('#'));
+		//net = netPay(gross, tax); 
+		name = line.substr(0, line.find('#'));
 		//delimiter = name.length() + 1;
 		//rate = line.substr(delimiter, line.find(' '));
 		//hours = line.substr(delimiter + 4, delimiter + 5);	
@@ -32,18 +32,26 @@ int main()
 		//delimiter = delimiter + hours.length() + 1;
 		//tax = line.substr(9,line.find('\0')); 
 		if (name.length() > 1){
-		cout << left << setw(25) << name << setw(20) << left << rate << setw(10) << hours << setw(10) << tax << setw(10) << gross << setw(10) << net << endl;
+		cout << left << setw(25) << name << setw(20) << left << rate << setw(10) << hours << setw(10) << tax << setw(10) << gross  << setw(10);
+		netPay(gross, tax);
 		}
 	} 
 	payrollFile.close();
 	return 0;
 }
 
-void grossPay (double payRate, int hoursWorked) {
-	return payRate * hoursWorked;
+double grossPay (double payRate, int hoursWorked) {
+	double total;
+	total = payRate * hoursWorked;
+	return total;
+	
 }
 void netPay(double grossAmt, double taxRate) {
-	double rate;
-	rate = (100 - taxRate)/100;
-	return grossAmt * rate;
+	double rate, total;
+	
+	rate = (100 - taxRate);
+	rate = rate/100;
+	total = grossAmt * rate;
+	cout << fixed << showpoint;
+	cout << setprecision(2) << total << endl;;
 	}
