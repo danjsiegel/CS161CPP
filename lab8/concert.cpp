fa4c0066f98850c2cb7750be32b9a4094a96ff69
Dan <dan.j.siegel@gmail.com>
2017-03-12 11:04:14 -0700

    committing because of weird error on g++

--- lab8/concert.cpp
+++ lab8/concert.cpp
@@ -1,32 +1,39 @@
 #include <iostream>
 #include <string>
+#include <ctype>
 
 
 using namespace std;
 
-enum {AVAILABLE, SOLD};
-enum {FLOOR, BALCONY, UPPER_BALCONY};
-enum {THURSDAY, FRIDAY, SATURDAY};
+enum ticketStatus {AVAILABLE, SOLD};
+enum ticketFloor {FLOOR, BALCONY, UPPER_BALCONY};
+enum ticketDate {THURSDAY, FRIDAY, SATURDAY};
 int MAX_SEATS = 20;
-char indicator[2] = {'*','S'};
+void ticketfunct();
 
-char ticket[UPPER_BALCONY+1][SATURDAY+1][MAX_SEATS];
+int main(){
+	//ticketfunct();
+	string seatInput;
+	int userSeat, userFloor;
 
-for(int section = FLOOR; section <= UPPER_BALCONY; section++){
+	while (seatInput != "x"){
+	seatInput = "TEST";
+	seatInput = tolower(seatInput);
+	cout << seatInput << endl;
+	
+	}
+
+	return 0;
+}
+
+
+void ticketfunct() {int ticket[UPPER_BALCONY+1][SATURDAY+1][MAX_SEATS];
+	for(int section = FLOOR; section <= UPPER_BALCONY; section++){
 	for(int day = THURSDAY; day <=SATURDAY; day++){
 		for(int seat = 0; seat < MAX_SEATS; seat++){
-			ticket[section][day][seat] = indicator[AVAILABLE];
-			cout << ticket[section][day][seat] << endl;
+			ticket[section][day][seat] = AVAILABLE;
+			cout << ticket[section][day][seat] << " ";
 			}
 		}
 	}
-
-
-
-
-
-int main(){
-	
-	
-	return 0;
 }
